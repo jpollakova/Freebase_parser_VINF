54c73b2e267de1ff0aec034677c91ef116695977
jpollakova <janepollakova@gmail.com>
2020-11-20 20:01:37 +0100

    Data preparation and basic search done

--- .vscode/data_preparing.py
+++ .vscode/data_preparing.py
@@ -1,9 +1,7 @@
 import re
 from itertools import islice
-
-#def parse_all_names():
-
-
+import pickle
+import json
 
 def create_artist_dict():
     f = open('artist_id.txt', 'rt', encoding="utf-8")
@@ -16,6 +14,26 @@ def create_artist_dict():
 
     f.close()
     return artist_dict
+
+
+def create_artist_awards_dict():
+    f = open('award_id.txt', 'rt', encoding="utf-8")
+
+    artist_awards_dict = {}
+
+    for line in f:
+        line = line.strip('\n')
+        id_artist = line[:re.search(',',line).span()[0]]
+        id_award = line[re.search(',',line).span()[1]:]
+
+        if id_artist not in artist_awards_dict:
+            artist_awards_dict[id_artist] = []
+
+        artist_awards_dict[id_artist].append(id_award)
+
+    f.close()
+    return artist_awards_dict
+
 
 def create_awards_dicts():
 
@@ -31,17 +49,81 @@ def create_awards_dicts():
     f.close()
     return (award_id_dict,award_honor_id_dict)
 
-
+def save_dicts_to_pickles():
+    DICT_artist_name = open("dict_artist_name.pkl","wb")
+    pickle.dump(artist_dict,DICT_artist_name)
+    DICT_artist_name.close()
 
 
 #vytvorit dictionaries z artist_id a award_honor_id
 
 artist_dict = create_artist_dict()
+artist_awards_dict = create_artist_awards_dict()
 award_id_dict , award_honor_id_dict= create_awards_dicts()
 
+'''
 x=0
-for key, value in award_honor_id_dict.items() :
+for key, value in artist_awards_dict.items() :
     x = x+1
     print (key, value)
     if x == 100:
         break
+'''
+
+f_all_names = open('all_names.txt', 'rt', encoding="utf-8")
+
+for line in f_all_names:
+    line = line.strip('\n')
+    id = line[:re.search(',',line).span()[0]]
+    #print(id)
+
+    if id in artist_dict:
+        #uloz name k idcku
+        artist_dict[id].append(line[re.search(',',line).span()[1]:])
+        continue
+
+    if id in award_honor_id_dict:
+        award_honor_id_dict[id].append(line[re.search(',',line).span()[1]:])
+        continue
+
+f_all_names.close()
+
+'''
+x=0
+for key, value in artist_dict.items() :
+    x = x+1
+    print (key, value)
+    if x == 500:
+        break
+'''
+
+DICT_artist_name = open("dict_artist_name.pkl","wb")
+pickle.dump(artist_dict,DICT_artist_name)
+DICT_artist_name.close()
+
+DICT_award_honor_name = open("dict_award_honor_name.pkl","wb")
+pickle.dump(award_honor_id_dict,DICT_award_honor_name)
+DICT_award_honor_name.close()
+
+DICT_artist_awards = open("dict_artist_awards.pkl","wb")
+pickle.dump(artist_awards_dict,DICT_artist_awards)
+DICT_artist_awards.close()
+
+DICT_award_award_honor = open("dict_award_award_honor.pkl","wb")
+pickle.dump(award_id_dict,DICT_award_award_honor)
+DICT_award_award_honor.close()
+
+'''
+#DICT_artist_name = open("dict_artist_name.pkl", "rb")
+#outofpickledict = pickle.load(DICT_artist_name)
+#DICT_artist_name.close()
+
+x=0
+for key, value in outofpickledict.items() :
+    x = x+1
+    print (key, value)
+    if x == 500:
+        break
+
+
+'''
--- .vscode/search.py
+++ .vscode/search.py
@@ -1,6 +1,147 @@
-import gzip
+import pickle
+
+def get_id_artist(term_searched):
+
+    for key, values in artist_name.items():
+        for v in values:
+            if v == term_searched :
+                return key
+
+    return None
+
+def get_id_award_honor(term_searched):
+
+    for key, values in award_honor_name.items():
+        for v in values:
+            if v == term_searched :
+                return key
+
+    return None
+
+def get_id_award(id_award_honor):
+
+    for key, values in award_award_honor.items():
+        if values == id_award_honor:
+            return key
+
+    return None
+
+def get_list_of_artists_awards(id_artist):
+
+    list_id_awards = []
+    for key, values in artist_awards.items():
+        if key == id_artist :
+            list_id_awards = values
+
+    #print(list_id_awards)
+
+    list_id_award_honor = []
+    for aw in list_id_awards:
+        for key, values in award_award_honor.items():
+            if key == aw:
+                list_id_award_honor.append(award_award_honor[aw])
+
+    #print(list_id_award_honor)
+
+    list_awards_names = []
+    for aw in list_id_award_honor:
+        if aw in award_honor_name:
+            list_awards_names.append(award_honor_name[aw])
+
+    #print(list_awards_names)
+
+    return list_awards_names
+
+def get_list_of_artists_won_this_award(id_award):
+
+    list_artists_won_this_award = []
+    for key, values in artist_awards.items():
+        for v in values:
+            if v == id_award :
+                list_artists_won_this_award.append(key)
+
+    #print(list_artists_won_this_award)
+
+    list_artists_won_this_award_names = []
+    for ar in list_artists_won_this_award:
+        if ar in artist_name:
+            list_artists_won_this_award_names.append(artist_name[ar])
+
+    #print(list_artists_won_this_award_names)
+
+    return list_artists_won_this_award_names
 
 
+DICT_artist_name = open("dict_artist_name.pkl", "rb")
+artist_name = pickle.load(DICT_artist_name)
+DICT_artist_name.close()
 
-# This part of code opens the Freebase dump and reads the data by line. Each line is then checked by function for parsing.
+DICT_artist_awards = open("dict_artist_awards.pkl", "rb")
+artist_awards = pickle.load(DICT_artist_awards)
+DICT_artist_awards.close()
 
+DICT_award_award_honor = open("dict_award_award_honor.pkl", "rb")
+award_award_honor = pickle.load(DICT_award_award_honor)
+DICT_award_award_honor.close()
+
+DICT_award_honor_name = open("dict_award_honor_name.pkl", "rb")
+award_honor_name = pickle.load(DICT_award_honor_name)
+DICT_award_honor_name.close()
+
+'''
+x=0
+for key, value in artist_name.items() :
+    x = x+1
+    print (key, value)
+    if x == 500:
+        break
+'''
+print('\n________________________________________________________\n')
+
+term_searched = 'Anne Murray,es'
+print('SEARCHED TERM - ARTIST: ' + term_searched)
+id = get_id_artist(term_searched)
+
+if id == None :
+    print('The term does not occur in the dictionary')
+else:
+    print('ID is : ' + str(id))
+    his_awards = get_list_of_artists_awards(id)
+    print('\nThe artist ' + term_searched + ' has won these awards:')
+    for x in range(len(his_awards)):        
+        print(x+1, end = '. ')
+        print(his_awards[x])
+
+
+print('\n________________________________________________________\n')
+
+#term_searched = 'Billboard Music Award for Modern Rock Artist of the Year,en'
+term_searched = 'Country Music Association Award for Vocal Duo of the Year,en'
+print('SEARCHED TERM - AWARD: ' + term_searched)
+
+idcko_honor = get_id_award_honor(term_searched)
+id_awardu = get_id_award(idcko_honor)
+
+if idcko_honor == None or id_awardu == None:
+    print('Sorry, but this award does not occur in the database.')
+else:
+    print('ID award honor: ' + idcko_honor)
+    print('ID award id: ' + id_awardu)
+    list_of_artists_won_this_award = get_list_of_artists_won_this_award(id_awardu)
+
+    print('\nThese artists won the searched award:')
+    for x in range(len(list_of_artists_won_this_award)):
+        print(x+1, end = '. ')
+        print(list_of_artists_won_this_award[x])
+
+
+print('\n________________________________________________________\n')
+
+'''
+for key, value in artist_awards.items():
+    #print value
+    number_of_values = len([item for item in value if item])
+    if number_of_values > 2:
+        if key in artist_name:
+            print(key, artist_name[key])
+'''
